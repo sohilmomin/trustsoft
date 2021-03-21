1644375c46eb16335bbbfd61aa320367f339082d
sohilmomin <sohilmomin711@gmail.com>
2021-03-21 15:59:31 +0530

    make it better

--- app.js
+++ app.js
@@ -30,19 +30,20 @@ connect.then((db) => {
   console.log("Connected with MonogoDB");
 }, (err) => { console.log(err); });
 
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static('client/build'))
+  const path = require('path')
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static('task-app/build'))
-  const path = require('path')
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'task-app', 'build', 'index.html'))
-  })
-}
+
 
 // error handler
 app.use(function (err, req, res, next) {
